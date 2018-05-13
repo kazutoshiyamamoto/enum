bbc7ce385365caf34f721ad7cc477f76443bc85b
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-05-13 23:57:32 +0900

    列挙型の各値に設定されている値を調べる

--- enum/enum/ViewController.swift
+++ enum/enum/ViewController.swift
@@ -8,32 +8,30 @@
 
 import UIKit
 
-enum WomensSize {
-    case XS, S, M, L
+
+enum Direction: Int {
+    case forward = 1
+    case backword
+    case right
+    case left
 }
 
 class ViewController: UIViewController {
-    
-    func packing(size:WomensSize) -> String {
-        var stuff:String
-        switch size {
-        case .XS, .S :
-            stuff = "女性用XS、Sサイズ備品"
-        case .M:
-            stuff = "女性用Mサイズ備品"
-        case .L:
-            stuff = "女性用Lサイズ備品"
-        }
-        return stuff
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let theStuff = packing(size: .M)
-        print(theStuff)
+        let muki1 = Direction.forward
+        let muki2 = Direction.backword
+        let muki3 = Direction.right
+        let muki4 = Direction.left
         
+        // Enum Valueに設定されているRaw Valueを調べる
+        print(muki1.rawValue)
+        print(muki2.rawValue)
+        print(muki3.rawValue)
+        print(muki4.rawValue)
     }
     
     override func didReceiveMemoryWarning() {
