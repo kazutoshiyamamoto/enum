f8e26ff25301dc49655c8dc026985b8c192c2be3
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-05-16 23:37:19 +0900

    列挙型にプロパティを定義

--- enum/enum/ViewController.swift
+++ enum/enum/ViewController.swift
@@ -8,20 +8,41 @@
 
 import UIKit
 
-// パターンの種類
-enum Pattern {
-    case Monotone(_: PColor)
-    case Border(color: PColor, color2: PColor)
-    case Dots(base: PColor, dot1: PColor, dot2: PColor)
+enum Ticket {
+    // チケットの種類
+    case Gold, A, B
+    // タイププロパティ
+    static var name = "入場券"
+    
+    // 席（リードオンリーの変数）
+    var area: String {
+        get {
+            switch self {
+            case .Gold:
+                return "ゴールド席"
+            case .A :
+                return "A席"
+            case .B:
+                return "B席"
+            }
+        }
+    }
+    
+    // 価格（リードオンリーの変数）
+    var price: Int {
+        get {
+            switch self {
+            case .Gold:
+                return 24000
+            case .A:
+                return 5000
+            case .B:
+                return 2000
+            }
+        }
+    }
 }
 
-// パターンで使える色
-enum PColor: String {
-    case red = "赤"
-    case green = "緑"
-    case yellow = "黄"
-    case white = "白"
-}
 
 class ViewController: UIViewController {
     
@@ -29,18 +50,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // チケット名の変更
+        Ticket.name = "超ライブ入場券"
         
+        // チケットを取得する
+        let ticket1 = Ticket.A
+        let ticket2 = Ticket.Gold
+        
+        // チケットの確認
+        print(Ticket.name, ticket1.area, ticket1.price)
+        print(Ticket.name, ticket2.area, ticket2.price)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    
-    
-    
-    
 }
 
